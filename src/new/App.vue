3a15e69e3b90c9827f7c46d742fc5eadea09819e
wuhaolei <nell53052@gmail.com>
2024-07-04 22:37:15 +0800

    entry

---
+++ src/new/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <ConfigProvider :theme="themeConfig">
+    <AppProvider>
+      <RouterView />
+    </AppProvider>
+  </ConfigProvider>
+</template>
+
+<script lang="ts" setup>
+  import { ConfigProvider } from 'ant-design-vue';
+  import { AppProvider } from '@/components/Application';
+  import { computed } from 'vue';
+  import { useDarkModeTheme } from '@/hooks/setting/useDarkModeTheme';
+
+  const { isDark, darkTheme } = useDarkModeTheme();
+
+  const themeConfig = computed(() =>
+    Object.assign(
+      {
+        token: {
+          colorPrimary: '#0960bd',
+          colorSuccess: '#55D187',
+          colorWarning: '#EFBD47',
+          colorError: '#ED6F6F',
+          colorInfo: '#0960bd',
+        },
+      },
+      isDark.value ? darkTheme : {},
+    ),
+  );
+</script>
